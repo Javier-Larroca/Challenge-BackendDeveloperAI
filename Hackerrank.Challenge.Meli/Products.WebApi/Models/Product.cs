56da36d7aa475153f84ef00bc75efb84c3acdcae
Javier Larroca <javier-larroca@hotmail.com>
2025-08-16 15:08:17 -0300

    Se agrega validaciones de DataAnnotation al product model

--- Hackerrank.Challenge.Meli/Products.WebApi/Models/Product.cs
+++ Hackerrank.Challenge.Meli/Products.WebApi/Models/Product.cs
@@ -1,13 +1,22 @@
-﻿namespace Products.WebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Products.WebApi.Models
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-        public string ImageUrl { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        [Required]
         public decimal Price { get; set; }
+        [Required]
         public double Rating { get; set; }
+        [Required]
         public string Specifications { get; set; }
     }
 }
